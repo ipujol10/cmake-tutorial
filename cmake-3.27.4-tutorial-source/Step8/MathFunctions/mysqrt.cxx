65784ab77ea6d6e29b2467c81dfdf662ee8e95a2
Iñaki Pujol <ipujol10@gmail.com>
2023-08-29 15:56:27 +0200

    UPDATE: step 8

--- cmake-3.27.4-tutorial-source/Step8/MathFunctions/mysqrt.cxx
+++ cmake-3.27.4-tutorial-source/Step8/MathFunctions/mysqrt.cxx
@@ -1,6 +1,6 @@
 #include "mysqrt.h"
+#include "Table.h"
 
-#include <cmath>
 #include <iostream>
 
 namespace mathfunctions {
@@ -13,12 +13,11 @@ double mysqrt(double x)
   }
 
   // if we have both log and exp then use them
-#if defined(HAVE_LOG) && defined(HAVE_EXP)
-  double result = std::exp(std::log(x) * 0.5);
-  std::cout << "Computing sqrt of " << x << " to be " << result
-            << " using log and exp" << std::endl;
-#else
   double result = x;
+  if (x >= 0 && x < 10) {
+      std::cout << "Use the table to help find an initial value\n";
+      result = sqrtTable[static_cast<int>(x)];
+  }
 
   // do ten iterations
   for (int i = 0; i < 10; ++i) {
@@ -29,7 +28,6 @@ double mysqrt(double x)
     result = result + 0.5 * delta / result;
     std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
   }
-#endif
   return result;
 }
 }
