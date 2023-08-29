399ffa3c3a0cdf33f979e6e46d5224cbe6d4e7a7
Iñaki Pujol <ipujol10@gmail.com>
2023-08-29 16:07:27 +0200

    UPDATE: step 9

---
+++ cmake-3.27.4-tutorial-source/Step9_build/MathFunctions/CMakeFiles/SqrtLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SqrtLibrary.dir/mysqrt.cxx.o"
+  "CMakeFiles/SqrtLibrary.dir/mysqrt.cxx.o.d"
+  "Table.h"
+  "libSqrtLibrary.a"
+  "libSqrtLibrary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SqrtLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
